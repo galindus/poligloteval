d1deb983f806b303b5388c00437fa3f24f339d5b
Victor Galindo <galindus@gmail.com>
2023-11-17 00:29:25 +0000

    cleanup

--- run_evals.sh
+++
@@ -1,4 +0,0 @@
-
-source ./venv/bin/activate
-jupyter nbconvert --to notebook --execute --ExecutePreprocessor.kernel_name=my-venv-kernel evals_catalanqa.ipynb
-jupyter nbconvert --to notebook --execute --ExecutePreprocessor.kernel_name=my-venv-kernel evals_xquad.ipynb
