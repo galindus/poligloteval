1f1203ff9986cefa3a66cb24eefb6fa99eef7dfd
root <galindus@gmail.com>
2023-11-22 08:22:52 +0000

    catalanqa results and wip madlad400 test

--- explore_translation_models.py
+++ explore_translation_models.py
@@ -5,57 +5,22 @@ os.environ['HF_HOME']='/workspace/.cache/huggingface'
 
 
 # %%
-from datasets import load_dataset
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from datasets import load_dataset
-import pyonmttok
-import ctranslate2
-from metrics import *
-from huggingface_hub import snapshot_download
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import time
 
 # %%
-print("Loading translator Models...")
-ca_en_model_folder = snapshot_download(repo_id="projecte-aina/mt-aina-ca-en", revision="main")
-tokenizer_ca_en = pyonmttok.Tokenizer(
-    mode="none", sp_model_path=ca_en_model_folder + "/spm.model"
-)
-ca_en_model = ctranslate2.Translator(ca_en_model_folder, device="cuda")
-en_ca_model_folder = snapshot_download(repo_id="projecte-aina/mt-aina-en-ca", revision="main")
-tokenizer_en_ca = pyonmttok.Tokenizer(
-    mode="none", sp_model_path=en_ca_model_folder + "/spm.model"
-)
-en_ca_model = ctranslate2.Translator(en_ca_model_folder, device="cuda")
+model_name = 'jbochi/madlad400-3b-mt'
+model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
+tokenizer = T5Tokenizer.from_pretrained(model_name, padding_side='left')
+
 # %%
-def translate_to_english(txt):
-    lines = [l for l in txt.split("\n") if l.strip() != ""]
+def translate_to(txt, language_code):
+    lines = [f"<2{language_code}> {l}" for l in txt.split("\n") if l.strip() != ""]
+    toks = tokenizer(lines, return_tensors="pt", padding=True).input_ids.to(model.device)
+    translated_toks = model.generate(input_ids=toks, max_new_tokens=2048)
+    translated_lines = tokenizer.batch_decode(translated_toks, skip_special_tokens=True)
+    return "\n".join(translated_lines)
 
-    toks, _ = tokenizer_ca_en.tokenize_batch(lines)
-    translated = ca_en_model.translate_batch(toks, max_input_length=2048, max_decoding_length=2048)
-    ts = []
-    for t in translated:
-        t_str = tokenizer_ca_en.detokenize(t.hypotheses[0])
-        # That is a bug on the translation outputing twice the translation.
-        if len(txt.split(" ")) == 1 and len(t_str.split(" ")) == 2:
-            t_str = t_str.split(" ")[0]
-        ts.append(t_str)
-
-    return "\n".join(ts)
-
-def translate_to_catalan(txt):
-    lines = [l for l in txt.split("\n") if l.strip() != ""]
-
-    toks, _ = tokenizer_en_ca.tokenize_batch(lines)
-    translated = en_ca_model.translate_batch(toks)
-    ts = []
-    for t in translated:
-        t_str = tokenizer_en_ca.detokenize(t.hypotheses[0])
-        # That is a bug on the translation outputing twice the translation.
-        if len(txt.split(" ")) == 1 and len(t_str.split(" ")) == 2:
-            t_str = t_str.split(" ")[0]
-        ts.append(t_str)
-
-    return "\n".join(ts)
 # %%
 example = """Algunes teories desenvolupades durant la dècada dels anys 70 van definir possibles vies a través de les quals la desigualtat pot tenir un efecte positiu en el desenvolupament econòmic. Segons una anàlisi del 1955, es pensava que els estalvis dels rics, si augmentaven amb la desigualtat, compensarien la reducció de la demanda dels consumidors. Un informe del 2013 sobre Nigèria suggereix que el creixement ha augmentat amb una desigualtat d’ingressos més gran. Algunes teories populars des de la dècada dels anys 50 fins al 2011 afirmaven, incorrectament, que la desigualtat tenia un efecte positiu en el desenvolupament econòmic. Les anàlisis basades en la comparació de les xifres anuals sobre igualtat amb els índexs de creixement anual van ser erronis ja que calen diversos anys perquè els efectes es manifestin com a canvis en el creixement econòmic. Els economistes de l’FMI van trobar una estreta relació entre nivells més baixos de desigualtat en els països en desenvolupament i períodes sostinguts de creixement econòmic. Els països en desenvolupament amb una elevada desigualtat han "aconseguit iniciar un creixement amb un índex elevat durant uns quants anys”, però "els períodes de creixement més llargs estan estretament relacionats amb més igualtat en la distribució de la renda".
 ----
@@ -71,11 +36,8 @@ Resposta: diversos anys
 Pregunta: A què s’associen els períodes de creixement més llargs?
 Resposta:"""
 
-example = 'Algunes teories desenvolupades durant la dècada dels anys 70 van definir possibles vies a través de les quals la desigualtat pot tenir un efecte positiu en el desenvolupament econòmic. Un informe del 2013 sobre Nigèria suggereix que el creixement ha augmentat amb una desigualtat d’ingressos més gran. Algunes teories populars des de la dècada dels anys 50 fins al 2011 afirmaven, incorrectament, que la desigualtat tenia un efecte positiu en el desenvolupament econòmic. Les anàlisis basades en la comparació de les xifres anuals sobre igualtat amb els índexs de creixement anual van ser erronis ja que calen diversos anys perquè els efectes es manifestin com a canvis en el creixement econòmic. Els economistes de l’FMI van trobar una estreta relació entre nivells més baixos de desigualtat en els països en desenvolupament i períodes sostinguts de creixement econòmic. Els països en desenvolupament amb una elevada desigualtat han "aconseguit iniciar un creixement amb un índex elevat durant uns quants anys”, però "els períodes de creixement més llargs estan estretament relacionats amb més igualtat en la distribució de la renda.'
-# example = 'Algunes teories desenvolupades durant la dècada dels anys 70 van definir possibles vies a través de les quals la desigualtat pot tenir un efecte positiu en el desenvolupament econòmic. Segons una anàlisi del 1955, es pensava que els estalvis dels rics, si augmentaven amb la desigualtat, compensarien la reducció de la demanda dels consumidors. Un informe del 2013 sobre Nigèria suggereix que el creixement ha augmentat amb una desigualtat d’ingressos més gran. Algunes teories populars des de la dècada dels anys 50 fins al 2011 afirmaven, incorrectament, que la desigualtat tenia un efecte positiu en el desenvolupament econòmic. Les anàlisis basades en la comparació de les xifres anuals sobre igualtat amb els índexs de creixement anual van ser erronis ja que calen diversos anys perquè els efectes es manifestin com a canvis en el creixement econòmic. Els economistes de l’FMI van trobar una estreta relació entre nivells més baixos de desigualtat en els països en desenvolupament i períodes sostinguts de creixement econòmic. Els països en desenvolupament amb una elevada desigualtat han "aconseguit iniciar un creixement amb un índex elevat durant uns quants anys”, però "els períodes de creixement més llargs estan estretament relacionats amb més igualtat en la distribució de la renda".'
-translated_en = translate_to_english(example)
-print(len(example))
-print(len(translated_en))
-print(translated_en[-100:])
-
-# %%
+#example = 'Algunes teories desenvolupades durant la dècada dels anys 70 van definir possibles vies a través de les quals la desigualtat pot tenir un efecte positiu en el desenvolupament econòmic. Un informe del 2013 sobre Nigèria suggereix que el creixement ha augmentat amb una desigualtat d’ingressos més gran. Algunes teories populars des de la dècada dels anys 50 fins al 2011 afirmaven, incorrectament, que la desigualtat tenia un efecte positiu en el desenvolupament econòmic. Les anàlisis basades en la comparació de les xifres anuals sobre igualtat amb els índexs de creixement anual van ser erronis ja que calen diversos anys perquè els efectes es manifestin com a canvis en el creixement econòmic. Els economistes de l’FMI van trobar una estreta relació entre nivells més baixos de desigualtat en els països en desenvolupament i períodes sostinguts de creixement econòmic. Els països en desenvolupament amb una elevada desigualtat han "aconseguit iniciar un creixement amb un índex elevat durant uns quants anys”, però "els períodes de creixement més llargs estan estretament relacionats amb més igualtat en la distribució de la renda.'
+start = time.time()
+translated_en = translate_to(example, "en")
+print(translated_en)
+print(f"Time: {time.time() - start}")
